0708c078464895b635ab99dbf8873ff6f491a196
gshaw-pivotal <gshaw+github@pivotal.io>
2018-03-27 22:26:41 -0400

    Remove unnecessary package in the scanBasePackage list

--- src/main/java/gs/nysub/NysubSlackbotApplication.java
+++ src/main/java/gs/nysub/NysubSlackbotApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "io.pivotal"})
+@SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot"})
 public class NysubSlackbotApplication {
 
 	public static void main(String[] args) {
